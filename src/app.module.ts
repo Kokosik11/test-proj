48be01790866057dd9031e3ec0ea1d092043e9f6
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2022-06-28 20:45:52 +0300

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'nestjs-prisma';
+import { prismaConfigAsync } from 'src/prisma.config';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRootAsync(prismaConfigAsync)],
   controllers: [AppController],
   providers: [AppService],
 })
